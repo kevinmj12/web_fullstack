d5da4c88aabcfab45a5efb977b4cfb6f99b2a40e
kevinmj12 <kevinmj12@gmail.com>
2025-02-26 18:21:05 +0900

    .env 환경변수 설정

--- backend/demo-api/youtube-demo/connection.js
+++ backend/demo-api/youtube-demo/connection.js
@@ -1,13 +1,12 @@
 const mysql = require("mysql2");
 
 require("dotenv").config();
-console.log(process.env.MYSQL_PASSWORD);
 
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   database: "board",
-  password: "", // 비밀번호 입력,
+  password: process.env.MYSQL_PASSWORD,
   dateStrings: true,
 });
 
