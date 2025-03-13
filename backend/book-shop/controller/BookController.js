67e0241d15722d18d4e0220259fac847075523c2
kevinmj12 <kevinmj12@gmail.com>
2025-03-14 00:15:37 +0900

    250313-페이지네이션

--- backend/book-shop/controller/BookController.js
+++ backend/book-shop/controller/BookController.js
@@ -3,39 +3,41 @@ const { StatusCodes } = require("http-status-codes");
 require("dotenv").config();
 
 const books = (req, res) => {
-  let { category_id } = req.query;
+  let categoryId = req.query.categoryid;
+  let isNew = req.query.isnew;
+  let { limit, page } = req.query;
+  limit = parseInt(limit);
+  page = parseInt(page);
 
-  if (!category_id) {
-    // 전체 도서 조회
-    const sql = "SELECT * FROM books;";
-
-    conn.query(sql, (err, results) => {
-      if (err) {
-        return res.status(StatusCodes.BAD_REQUEST).json({
-          msg: `Error: ${err.code}`,
-        });
-      }
-      return res.status(StatusCodes.OK).json(results);
-    });
-  } else {
-    // 카테고리별 도서 목록 조회
-    category_id = parseInt(category_id);
-
-    const sql = "SELECT * FROM books WHERE category_id=?;";
-
-    conn.query(sql, category_id, (err, results) => {
-      if (err) {
-        return res.status(StatusCodes.BAD_REQUEST).json({
-          msg: `Error: ${err.code}`,
-        });
-      }
-      if (results[0]) {
-        return res.status(StatusCodes.OK).json(results);
-      } else {
-        return res.status(StatusCodes.NOT_FOUND).end();
-      }
-    });
+  let sql = "SELECT * FROM books"; // 전체 도서 조회
+  let values = [];
+  if (categoryId && isNew) {
+    // 카테고리별 신간 도서 조회
+    sql = `SELECT * FROM books WHERE category_id = ? AND
+      pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()`;
+    values = [categoryId, isNew];
+  } else if (categoryId) {
+    // 카테고리별 조회
+    sql = "SELECT * FROM books WHERE category_id = ?";
+    values = [categoryId];
+  } else if (isNew) {
+    // 신간 조회
+    sql = `SELECT * FROM books WHERE 
+      pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()`;
+    values = [isNew];
   }
+
+  sql += ` LIMIT ${limit} OFFSET ${(page - 1) * limit};`;
+
+  console.log(sql);
+  conn.query(sql, values, (err, results) => {
+    if (err) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        msg: `Error: ${err.code}`,
+      });
+    }
+    return res.status(StatusCodes.OK).json(results);
+  });
 };
 
 // 개별 도서 조회
@@ -43,7 +45,8 @@ const bookDetail = (req, res) => {
   let { id } = req.params;
   id = parseInt(id);
 
-  const sql = "SELECT * FROM books WHERE id=?;";
+  const sql = `SELECT * FROM books LEFT JOIN categories
+  ON books.category_id = categories.id WHERE books.id=?;`;
 
   conn.query(sql, id, (err, results) => {
     if (err) {
