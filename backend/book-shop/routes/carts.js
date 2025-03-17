c7568df49c18536cc5d979460207603917f5f719
kevinmj12 <kevinmj12@gmail.com>
2025-03-17 22:53:40 +0900

    250317-장바구니

--- backend/book-shop/routes/carts.js
+++ backend/book-shop/routes/carts.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const {
+  addCartItems,
+  getCartItems,
+  removeCartItems,
+} = require("../controller/CartController");
 
 router.use(express.json());
 
+// 장바구니 추가
+router.post("/", addCartItems);
+
 // 장바구니 조회
-router.get("/", (req, res) => {});
+router.get("/", getCartItems);
 
 // 장바구니 도서 삭제
-router.delete("/:id", (req, res) => {});
+router.delete("/", removeCartItems);
 
 module.exports = router;
